616f865bbffc8696e5e8783c9165bd2acb1d41cc
Maksym Blank <blank.maksym@gmail.com>
2019-09-26 09:24:28 -0500

    Update the migrate script

--- dist/js/migrate.js
+++ dist/js/migrate.js
@@ -1,116 +1,155 @@
+// firebase.initializeApp({
+//     "apiKey": "AIzaSyAGgqla_TWV4wVgaQ87HG3VDT0igLA8KhU",
+//     "authDomain": "droneblocks-staging.firebaseapp.com",
+//     "databaseURL": "https://droneblocks-staging.firebaseio.com",
+//     "projectId": "droneblocks-staging",
+//     "storageBucket": "droneblocks-staging.appspot.com"
+// });
+
 firebase.initializeApp({
-    "apiKey": "AIzaSyAGgqla_TWV4wVgaQ87HG3VDT0igLA8KhU",
-    "authDomain": "droneblocks-staging.firebaseapp.com",
-    "databaseURL": "https://droneblocks-staging.firebaseio.com",
-    "projectId": "droneblocks-staging",
-    "storageBucket": "droneblocks-staging.appspot.com"
-});
+    "apiKey": "AIzaSyBcbAt69BGUjPiOucQbkcNNbcdaSmE-G0o",
+    "authDomain": "fiery-inferno-4972.firebaseapp.com",
+    "databaseURL": "https://fiery-inferno-4972.firebaseio.com",
+    "projectId": "fiery-inferno-4972",
+    "storageBucket": "fiery-inferno-4972.appspot.com"
+ });
 
 const db = firebase.firestore();
 const rdb = firebase.database();
 
-return;
+// rdb.ref('droneblocks/tello_missions').on('value', snap => {
+//     const missions = []
+//     snap.forEach((v) => {
+//         const mis = v.val();
+//         missions.push({
+//             createdAt: new Date(mis.createdAt),
+//             title: mis.title,
+//             missionXML: mis.missionXML,
+//             uid: mis.userId,
+//             aircraft: 'Tello'
+//         });
+//     })
 
-rdb.ref('droneblocks/tello_missions').on('value', snap => {
-    const missions = []
-    snap.forEach((v) => {
-        const mis = v.val();
-        missions.push({
-            createdAt: new Date(mis.createdAt),
-            title: mis.title,
-            missionXML: mis.missionXML,
-            uid: mis.userId,
-            aircraft: 'Tello'
+//     missions.forEach((mission) => {
+//         const _missionId = Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2);
+
+//         // db.collection('missions').doc(_missionId).set(mission);
+//     })
+// })
+
+// rdb.ref('droneblocks/missions').on('value', snap => {
+//     const missions = []
+//     snap.forEach((v) => {
+//         const mis = v.val();
+//         missions.push({
+//             createdAt: new Date(mis.createdAt),
+//             title: mis.title,
+//             missionXML: mis.missionXML,
+//             uid: mis.userId,
+//             aircraft: 'DJI'
+//         });
+//     })
+
+//     missions.forEach((mission) => {
+//         const _missionId = Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2);
+
+//         // db.collection('missions').doc(_missionId).set(mission);
+//     })
+// })
+
+(async() => {
+    return;
+    
+    let users = await new Promise((resolve) => {
+        rdb.ref('droneblocks/users').on('value', async snap => {
+            const users = []
+            snap.forEach((v) => {
+                users.push({
+                    ...v.val(),
+                    id: v.ref.key
+                });
+            })
+
+            resolve(users);
         });
     })
 
-    missions.forEach((mission) => {
-        const _missionId = Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2);
+    let i = 0;
 
-        db.collection('missions').doc(_missionId).set(mission);
-    })
-})
+    users = users.slice(9000);
 
-rdb.ref('droneblocks/missions').on('value', snap => {
-    const missions = []
-    snap.forEach((v) => {
-        const mis = v.val();
-        missions.push({
-            createdAt: new Date(mis.createdAt),
-            title: mis.title,
-            missionXML: mis.missionXML,
-            uid: mis.userId,
-            aircraft: 'DJI'
-        });
-    })
+    console.log(users.length);
 
-    missions.forEach((mission) => {
-        const _missionId = Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2);
+    for(let v of users){
+        if(!v.displayName || !v.email || !v.loginAt || !v.createdAt || !v.photoURL || !v.providerId || !v.uid){
+            console.warn('User error', v);
+        }
 
-        db.collection('missions').doc(_missionId).set(mission);
-    })
-})
+        try{
+            // await db.collection('users').doc(v.id).set({
+            //     createdAt: new Date(v.createdAt) || new Date(),
+            //     displayName: v.displayName || '',
+            //     email: v.email || '',
+            //     loginAt: new Date(v.loginAt) || new Date(),
+            //     photoURL: v.photoURL || '',
+            //     providerId: v.providerId || '',
+            //     uid: v.uid || ''
+            // });
 
-rdb.ref('droneblocks/users').on('value', snap => {
-    const users = []
-    snap.forEach((v) => {
-        users.push({
-            ...v.val(),
-            id: v.ref.key
-        });
-    })
+            console.log('Migrated user', i, v.id);
+        }catch(e){
+            console.error(e);
+        }
 
-    users.forEach((v, i) => {
-        // if(v.missions){
-        //     for(let key in v.tello_missions){
-        //         new Promise(async(resolve, reject) => {
-        //             const Tellomission = await rdb.ref(`droneblocks/tello_missions/${key}`).once('value');
+        i++;
+    }
 
-        //             const data = Tellomission.val();
+    console.log('Done');
+})();
 
-        //             resolve({
-        //                 createdAt: new Date(data.createdAt),
-        //                 aircraft: 'Tello',
-        //                 uid: data.userId,
-        //                 title: data.title,
-        //                 missionXML: data.missionXML
-        //             })
-        //         }).then((mission) => {
-        //             const _missionId = Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2);
+// rdb.ref('droneblocks/users').on('value', async snap => {
+//     const users = []
+//     snap.forEach((v) => {
+//         users.push({
+//             ...v.val(),
+//             id: v.ref.key
+//         });
+//     })
 
-        //             // db.collection('missions').doc(_missionId).set(mission);
-        //         })
-        //     }
-        //     for(let key in v.missions){
-        //         new Promise(async (resolve, reject) => {
-        //             const DJImission = await rdb.ref(`droneblocks/missions/${key}`).once('value');
-        //             const data = DJImission.val();
+//     let i = 0;
 
-        //             resolve({
-        //                 createdAt: new Date(data.createdAt),
-        //                 aircraft: 'DJI',
-        //                 uid: data.userId,
-        //                 title: data.title,
-        //                 missionXML: data.missionXML
-        //             })
-        //         }).then((mission) => {
-        //             const _missionId = Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2);
+//     for(let v of users){
+//         if(!v.displayName || !v.email || !v.loginAt || !v.createdAt || !v.photoURL || !v.providerId || !v.uid){
+//             console.warn('User error', v);
+//         }
 
-        //             // db.collection('missions').doc(_missionId).set(mission);
-        //         })
-        //     }
-        // }
+//         // await db.collection('users').doc(v.id).set({
+//         //     createdAt: new Date(v.createdAt) || new Date(),
+//         //     displayName: v.displayName || '',
+//         //     email: v.email || '',
+//         //     loginAt: new Date(v.loginAt) || new Date(),
+//         //     photoURL: v.photoURL || '',
+//         //     providerId: v.providerId || '',
+//         //     uid: v.uid
+//         // });
 
-        db.collection('users').doc(v.id).set({
-            createdAt: new Date(v.createdAt),
-            displayName: v.displayName || '',
-            email: v.email,
-            loginAt: new Date(v.loginAt),
-            photoURL: v.photoURL,
-            providerId: v.providerId,
-            uid: v.uid
-        });
+//         console.log('Migrated user', i, v.id);
 
-        console.log('Migrated user', i, v.id);
-    })
-});
+//         i++;
+//     }
+
+//     console.log('Done');
+
+//     // users.forEach((v, i) => {
+//         // db.collection('users').doc(v.id).set({
+//         //     createdAt: new Date(v.createdAt),
+//         //     displayName: v.displayName || '',
+//         //     email: v.email,
+//         //     loginAt: new Date(v.loginAt),
+//         //     photoURL: v.photoURL,
+//         //     providerId: v.providerId,
+//         //     uid: v.uid
+//         // });
+
+//     // })
+// });
