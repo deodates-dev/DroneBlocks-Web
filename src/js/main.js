be08a21b6d2660b24d1bbf1303cd9d558d4fb1ba
Dennis Baldwin <dennisbaldwin@gmail.com>
2019-11-22 13:25:58 -0600

    Remove DJI option for Android

--- src/js/main.js
+++ src/js/main.js
@@ -50,12 +50,6 @@ const bind = () => {
     }
   
     $("#codeView").addClass("hidden");
-    
-    // For now let's not show an option to connect to DJI since Android only supports Tello
-    if(userAgent.match( /Android/i ) && aircraft == "Tello") {
-      $("#connectTo").parent().remove();
-      $("#d4").remove();
-    }
 
     $('.button-collapse').sideNav({
         edge: 'right',
