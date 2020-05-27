2df9bce0d5f6c78fc59e23434a139ae29fe0e4bf
Dennis Baldwin <dennisbaldwin@gmail.com>
2020-05-27 13:32:07 -0500

    Fix issue with clicking on missions from tello missions list sent to DJI blocks. Now sends to Tello blocks.

--- src/js/missions.js
+++ src/js/missions.js
@@ -68,16 +68,15 @@ $(document).ready(() => {
         select: function(id) {
           localStorage.removeItem('backup');
           localStorage.setItem('missionId', id);
-
-          // Trigger off chrome_app url param
-          if (location.search.indexOf('chrome_app') > -1) {
-            location.href = '/chrome_app.html';
-          } else if (location.search.indexOf('simulator') > -1) {
+          
+          if (location.search.indexOf('simulator') > -1) {
             location.href = '/simulator.html';
+          } else if (location.search.indexOf('chrome_app') > -1 || aircraft === 'Tello') {
+            location.href = '/chrome_app.html';
           } else {
             location.href = '/';
           }
-          //location.href = aircraft === 'Tello' ? '/chrome_app.html' : '/';
+          
         },
         removeItem: function(id) {
           const that = this;
