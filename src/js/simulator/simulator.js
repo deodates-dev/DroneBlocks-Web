a7ca6e27d8f6af4a2b3ef38d3db299b1e433a854
temp2020 <chowdhurysunyad@gmail.com>
2020-08-06 09:59:31 -0400

    minecraft terrain

--- src/js/simulator/simulator.js
+++ src/js/simulator/simulator.js
@@ -83,6 +83,11 @@ var ringsPosConfig = [
 const MAX_PATH_POINTS = 100000;
 let positionBuffer = {};
 
+var worldWidth = 200, worldDepth = 200;
+var worldHalfWidth = worldWidth / 2;
+var worldHalfDepth = worldDepth / 2;
+var data = generateHeight(worldWidth, worldDepth);
+
 scene = new THREE.Scene();
 scene.position.y = scene.position.y - 2000 // Lower original axis
 scene.background = new THREE.Color(0xcccccc);
@@ -123,7 +128,7 @@ document.getElementById("droneArea").appendChild(renderer.domElement);
 // automatically resize renderer
 THREEx.WindowResize(renderer, camera);
 // toggle full-screen on given key press
-//THREEx.FullScreen.bindKey({ charCode: 'm'.charCodeAt(0) });
+// THREEx.FullScreen.bindKey({ charCode: 'm'.charCodeAt(0) });
 controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.minDistance = 200;
 controls.maxDistance = 60000;
@@ -174,11 +179,11 @@ var material = new THREE.MeshBasicMaterial({
   color: 0x999999, // Ground Color determine.
 })
 
-var mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(100000, 100000), material);
-mesh.position.y = 0;
+/* var mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(100000, 100000), material);
+mesh.position.y = 1;
 mesh.rotation.x = - Math.PI / 2;
 mesh.receiveShadow = true;
-scene.add(mesh);
+scene.add(mesh); */
 
 
 
@@ -272,6 +277,194 @@ const pathLine = new THREE.Line(pathGeometry, pathMaterial);
 pathLine.computeLineDistances();
 pathLine.geometry.dynamic = true;
 scene.add(pathLine);
+
+// MineCraft Terrain
+var mineLight = new THREE.Color( 0xffffff );
+var mineShadow = new THREE.Color( 0x505050 );
+
+var matrix = new THREE.Matrix4();
+
+var pxGeometry = new THREE.PlaneGeometry( 100, 100 );
+pxGeometry.faces[ 0 ].vertexColors = [ mineLight, mineShadow, mineLight ];
+pxGeometry.faces[ 1 ].vertexColors = [ mineShadow, mineShadow, mineLight ];
+pxGeometry.faceVertexUvs[ 0 ][ 0 ][ 0 ].y = 0.5;
+pxGeometry.faceVertexUvs[ 0 ][ 0 ][ 2 ].y = 0.5;
+pxGeometry.faceVertexUvs[ 0 ][ 1 ][ 2 ].y = 0.5;
+pxGeometry.rotateY( Math.PI / 2 );
+pxGeometry.translate( 50, 0, 0 );
+
+var nxGeometry = new THREE.PlaneGeometry( 100, 100 );
+nxGeometry.faces[ 0 ].vertexColors = [ mineLight, mineShadow, mineLight ];
+nxGeometry.faces[ 1 ].vertexColors = [ mineShadow, mineShadow, mineLight ];
+nxGeometry.faceVertexUvs[ 0 ][ 0 ][ 0 ].y = 0.5;
+nxGeometry.faceVertexUvs[ 0 ][ 0 ][ 2 ].y = 0.5;
+nxGeometry.faceVertexUvs[ 0 ][ 1 ][ 2 ].y = 0.5;
+nxGeometry.rotateY( - Math.PI / 2 );
+nxGeometry.translate( - 50, 0, 0 );
+
+var pyGeometry = new THREE.PlaneGeometry( 100, 100 );
+pyGeometry.faces[ 0 ].vertexColors = [ mineLight, mineLight, mineLight ];
+pyGeometry.faces[ 1 ].vertexColors = [ mineLight, mineLight, mineLight ];
+pyGeometry.faceVertexUvs[ 0 ][ 0 ][ 1 ].y = 0.5;
+pyGeometry.faceVertexUvs[ 0 ][ 1 ][ 0 ].y = 0.5;
+pyGeometry.faceVertexUvs[ 0 ][ 1 ][ 1 ].y = 0.5;
+pyGeometry.rotateX( - Math.PI / 2 );
+pyGeometry.translate( 0, 50, 0 );
+
+var py2Geometry = new THREE.PlaneGeometry( 100, 100 );
+py2Geometry.faces[ 0 ].vertexColors = [ mineLight, mineLight, mineLight ];
+py2Geometry.faces[ 1 ].vertexColors = [ mineLight, mineLight, mineLight ];
+py2Geometry.faceVertexUvs[ 0 ][ 0 ][ 1 ].y = 0.5;
+py2Geometry.faceVertexUvs[ 0 ][ 1 ][ 0 ].y = 0.5;
+py2Geometry.faceVertexUvs[ 0 ][ 1 ][ 1 ].y = 0.5;
+py2Geometry.rotateX( - Math.PI / 2 );
+py2Geometry.rotateY( Math.PI / 2 );
+py2Geometry.translate( 0, 50, 0 );
+
+var pzGeometry = new THREE.PlaneGeometry( 100, 100 );
+pzGeometry.faces[ 0 ].vertexColors = [ mineLight, mineShadow, mineLight ];
+pzGeometry.faces[ 1 ].vertexColors = [ mineShadow, mineShadow, mineLight ];
+pzGeometry.faceVertexUvs[ 0 ][ 0 ][ 0 ].y = 0.5;
+pzGeometry.faceVertexUvs[ 0 ][ 0 ][ 2 ].y = 0.5;
+pzGeometry.faceVertexUvs[ 0 ][ 1 ][ 2 ].y = 0.5;
+pzGeometry.translate( 0, 0, 50 );
+
+var nzGeometry = new THREE.PlaneGeometry( 100, 100 );
+nzGeometry.faces[ 0 ].vertexColors = [ mineLight, mineShadow, mineLight ];
+nzGeometry.faces[ 1 ].vertexColors = [ mineShadow, mineShadow, mineLight ];
+nzGeometry.faceVertexUvs[ 0 ][ 0 ][ 0 ].y = 0.5;
+nzGeometry.faceVertexUvs[ 0 ][ 0 ][ 2 ].y = 0.5;
+nzGeometry.faceVertexUvs[ 0 ][ 1 ][ 2 ].y = 0.5;
+nzGeometry.rotateY( Math.PI );
+nzGeometry.translate( 0, 0, - 50 );
+
+var geometry = new THREE.Geometry();
+
+				for ( var z = 0; z < worldDepth; z ++ ) {
+
+					for ( var x = 0; x < worldWidth; x ++ ) {
+
+						var h = getY( x, z );
+
+						matrix.makeTranslation(
+							x * 100 - worldHalfWidth * 100,
+							h * 100,
+							z * 100 - worldHalfDepth * 100
+						);
+
+						var px = getY( x + 1, z );
+						var nx = getY( x - 1, z );
+						var pz = getY( x, z + 1 );
+						var nz = getY( x, z - 1 );
+
+						var pxpz = getY( x + 1, z + 1 );
+						var nxpz = getY( x - 1, z + 1 );
+						var pxnz = getY( x + 1, z - 1 );
+						var nxnz = getY( x - 1, z - 1 );
+
+						var a = nx > h || nz > h || nxnz > h ? 0 : 1;
+						var b = nx > h || pz > h || nxpz > h ? 0 : 1;
+						var c = px > h || pz > h || pxpz > h ? 0 : 1;
+						var d = px > h || nz > h || pxnz > h ? 0 : 1;
+
+						if ( a + c > b + d ) {
+
+							var colors = py2Geometry.faces[ 0 ].vertexColors;
+							colors[ 0 ] = b === 0 ? mineShadow : mineLight;
+							colors[ 1 ] = c === 0 ? mineShadow : mineLight;
+							colors[ 2 ] = a === 0 ? mineShadow : mineLight;
+
+							var colors = py2Geometry.faces[ 1 ].vertexColors;
+							colors[ 0 ] = c === 0 ? mineShadow : mineLight;
+							colors[ 1 ] = d === 0 ? mineShadow : mineLight;
+							colors[ 2 ] = a === 0 ? mineShadow : mineLight;
+
+							geometry.merge( py2Geometry, matrix );
+
+						} else {
+
+							var colors = pyGeometry.faces[ 0 ].vertexColors;
+							colors[ 0 ] = a === 0 ? mineShadow : mineLight;
+							colors[ 1 ] = b === 0 ? mineShadow : mineLight;
+							colors[ 2 ] = d === 0 ? mineShadow : mineLight;
+
+							var colors = pyGeometry.faces[ 1 ].vertexColors;
+							colors[ 0 ] = b === 0 ? mineShadow : mineLight;
+							colors[ 1 ] = c === 0 ? mineShadow : mineLight;
+							colors[ 2 ] = d === 0 ? mineShadow : mineLight;
+
+							geometry.merge( pyGeometry, matrix );
+
+						}
+
+						if ( ( px != h && px != h + 1 ) || x == 0 ) {
+
+							var colors = pxGeometry.faces[ 0 ].vertexColors;
+							colors[ 0 ] = pxpz > px && x > 0 ? mineShadow : mineLight;
+							colors[ 2 ] = pxnz > px && x > 0 ? mineShadow : mineLight;
+
+							var colors = pxGeometry.faces[ 1 ].vertexColors;
+							colors[ 2 ] = pxnz > px && x > 0 ? mineShadow : mineLight;
+
+							geometry.merge( pxGeometry, matrix );
+
+						}
+
+						if ( ( nx != h && nx != h + 1 ) || x == worldWidth - 1 ) {
+
+							var colors = nxGeometry.faces[ 0 ].vertexColors;
+							colors[ 0 ] = nxnz > nx && x < worldWidth - 1 ? mineShadow : mineLight;
+							colors[ 2 ] = nxpz > nx && x < worldWidth - 1 ? mineShadow : mineLight;
+
+							var colors = nxGeometry.faces[ 1 ].vertexColors;
+							colors[ 2 ] = nxpz > nx && x < worldWidth - 1 ? mineShadow : mineLight;
+
+							geometry.merge( nxGeometry, matrix );
+
+						}
+
+						if ( ( pz != h && pz != h + 1 ) || z == worldDepth - 1 ) {
+
+							var colors = pzGeometry.faces[ 0 ].vertexColors;
+							colors[ 0 ] = nxpz > pz && z < worldDepth - 1 ? mineShadow : mineLight;
+							colors[ 2 ] = pxpz > pz && z < worldDepth - 1 ? mineShadow : mineLight;
+
+							var colors = pzGeometry.faces[ 1 ].vertexColors;
+							colors[ 2 ] = pxpz > pz && z < worldDepth - 1 ? mineShadow : mineLight;
+
+							geometry.merge( pzGeometry, matrix );
+
+						}
+
+						if ( ( nz != h && nz != h + 1 ) || z == 0 ) {
+
+							var colors = nzGeometry.faces[ 0 ].vertexColors;
+							colors[ 0 ] = pxnz > nz && z > 0 ? mineShadow : mineLight;
+							colors[ 2 ] = nxnz > nz && z > 0 ? mineShadow : mineLight;
+
+							var colors = nzGeometry.faces[ 1 ].vertexColors;
+							colors[ 2 ] = nxnz > nz && z > 0 ? mineShadow : mineLight;
+
+							geometry.merge( nzGeometry, matrix );
+
+						}
+
+					}
+
+				}
+    geometry = new THREE.BufferGeometry().fromGeometry( geometry );
+
+    var texture = new THREE.TextureLoader().load( 'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/minecraft/atlas.png' );
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.LinearMipmapLinearFilter;
+
+    var minecraftMesh = new THREE.Mesh(
+      geometry,
+      new THREE.MeshLambertMaterial( { map: texture, vertexColors: true, side: THREE.DoubleSide } )
+);
+    console.log(minecraftMesh.position)
+    scene.add( minecraftMesh );
+
 
 let then = 0;
 
@@ -932,6 +1125,30 @@ $(document).keypress(function (e) {
 
 });
 
+function generateHeight( width, height ) {
+
+  var data = [], perlin = new ImprovedNoise(),
+    size = width * height, quality = 2, z = Math.random() * 100;
+
+  for ( var j = 0; j < 4; j ++ ) {
+
+    if ( j == 0 ) for ( var i = 0; i < size; i ++ ) data[ i ] = 0;
+
+    for ( var i = 0; i < size; i ++ ) {
+
+      var x = i % width, y = ( i / width ) | 0;
+      data[ i ] += perlin.noise( x / quality, y / quality, z ) * quality;
+
+    }
+
+    quality *= 4;
+
+  }
+
+  return data;
+
+}
+
 $( document ).ready(function() {
   $("#toogleSound").click(() => {
     if (isFlying && !!!sound.isPlaying) {
@@ -964,4 +1181,10 @@ function initPath() {
   }
   pathLine.geometry.setDrawRange(0, MAX_PATH_POINTS);
   pathLine.geometry.attributes.position.needsUpdate = true;
+}
+
+function getY( x, z ) {
+
+  return ( data[ x + z * worldWidth ] * 0.2 ) | 0;
+
 }
