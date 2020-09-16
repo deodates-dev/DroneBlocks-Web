bd7463fd5bc708d0c90434d4464af268e58e9cac
temp2020 <chowdhurysunyad@gmail.com>
2020-09-16 03:39:11 -0400

    add initial admin page

--- src/js/missions.js
+++ src/js/missions.js
@@ -2,7 +2,7 @@ import Vue from '../lib/vue/vue.min';
 import * as moment from 'moment';
 
 $(document).ready(() => {
-  if(!location.pathname.match(/missions.html/) && !location.pathname.match(/code_board.html/)){
+  if(!location.pathname.match(/missions.html/) && !location.pathname.match(/code_board.html/) && !location.pathname.match(/droneblocks-admin/)){
     return;
   }
   var randomColors = [
@@ -184,6 +184,230 @@ $(document).ready(() => {
                     <label for="recenMissionFirst">Recent</label>
                     <input class="with-gap" name="group1" type="radio" id="highRatingFirst" />
                     <label for="highRatingFirst">Popular</label>
+                  </p>
+                </form>
+              </div>
+              <div class="row" v-if="missions.length">
+                <div class="col s12 m3" v-for="(mission, index) in missions" :key="mission.id">
+                  <div class="card small">
+                    <div class="card-image" :id="index">
+                      <h5>
+                        {{ mission.title }}
+                      </h5>
+                    </div>
+                    <div class="card-content">
+                      <p>
+                        Created : {{ mission.createdAt }}
+                      </p>
+                      <div class="publicMissionShare">
+                        <h5 class="likeCount"><small>{{ mission.likeCount || 0 }}</small></h5>
+                        <a v-on:click="likeMission(mission)" class="btn-floating halfway-fab waves-effect waves-light pink iconMission"><i class="material-icons">favorite</i>123</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div v-else class="center-align pt-20">
+                <i v-if="filterIndex > -1" class="fa fa-spinner fa-spin fa-2x"></i>
+                <p v-else>You don't have any missions.</p>
+              </div>
+            </div>
+          </div>
+        </template>
+        <div v-else class="center-align pt-20">
+          <i class="fa fa-spinner fa-spin fa-2x"></i>
+        </div>`,
+      data: {
+        missions: undefined,
+        filterIndex: -1,
+      },
+      mounted: function(){
+        this.getData();
+      },
+      updated: function () {
+        this.$nextTick(function () {
+          var container = this;
+          this.missions.map((mission, index) => {
+            $(`#${index}`).css("background-color", cardColors[index]);
+            $(`#${index}`).click(function(){
+              window.location = `/simulator.html?share=1&missionId=${mission.id}&uid=${mission.uid}`;
+            });
+          });
+          $("#recenMissionFirst").on("change", function () {
+            var status = $(this).prop('checked');
+            if (status === true) {
+              container.missions = [];
+              container.filterIndex = 0;
+              container.getData();
+            }
+          });
+
+          $("#highRatingFirst").on("change", function () {
+            var status = $(this).prop('checked');
+            if (status === true) {
+              container.missions = [];
+              container.filterIndex = 1;
+              container.getData();
+            }
+          });
+        })
+      },
+      methods: {
+        select: function(id) {
+          localStorage.removeItem('backup');
+          localStorage.setItem('missionId', id);
+          
+          if (location.search.indexOf('simulator') > -1) {
+            location.href = '/simulator.html';
+          } else if (location.search.indexOf('chrome_app') > -1 || aircraft === 'Tello') {
+            location.href = '/chrome_app.html';
+          } else {
+            location.href = '/';
+          }
+          
+        },
+        removeItem: function(id) {
+          const that = this;
+
+          $('#deleteMissionModal').openModal();
+
+          $('#deleteMissionButton').off().click(() => {
+            db.collection('missions').doc(id).delete();
+
+            $('#deleteMissionModal').closeModal();
+            that.getData();
+
+            if(localStorage.getItem('missionId') === id){
+              localStorage.removeItem('missionId');
+            }
+          })
+        },
+        share: function(id){
+          $("#shareModal").openModal();
+  
+          if (location.search.indexOf('simulator') > -1) {
+            $("#iPadShareLink").val(`droneblocks://?missionId=${id}&uid=${user.uid}&aircraft=tello`);
+            $("#desktopShareLink").val(`https://dev.droneblocks.io/simulator.html?share=1&missionId=${id}&uid=${user.uid}`);
+          } else if (aircraft == "Tello") {
+            $("#iPadShareLink").val(`droneblocks://?missionId=${id}&uid=${user.uid}&aircraft=tello`);
+            $("#desktopShareLink").val(`https://dev.droneblocks.io/tello.html?share=1&missionId=${id}&uid=${user.uid}`);
+          } else {
+            $("#iPadShareLink").val(`droneblocks://?missionId=${id}&uid=${user.uid}`);
+            $("#desktopShareLink").val(`https://dev.droneblocks.io?share=1&missionId=${id}&uid=${user.uid}`);
+          }
+        },
+        likeMission: function (myMission) {
+          if (!user) {
+            $("#redirectLoginModal").openModal();
+            return;
+          }
+          if (!myMission.likeMembers || (myMission.likeMembers.length === 0)) {
+            db.collection('missions').doc(myMission.id).update({
+              likeCount: 1,
+              likeMembers: [user.uid]
+            }).then(() => {
+              Materialize.toast(`You liked this mission.`, 3000);
+              this.missions.map(mission => {
+                if (mission.id === myMission.id) {
+                  mission.likeCount = 1;
+                  mission.likeMembers = [user.uid];
+                }
+              });
+            });
+            return;
+          }
+          if (!myMission.likeMembers.includes(user.uid)) {
+            db.collection('missions').doc(myMission.id).update({
+              likeCount: firebase.firestore.FieldValue.increment(1),
+              likeMembers: firebase.firestore.FieldValue.arrayUnion(user.uid),
+            }).then(() => {
+              Materialize.toast(`You liked this mission`, 3000);
+              this.missions.map(mission => {
+                if (mission.id === myMission.id) {
+                  mission.likeCount++;
+                  mission.likeMembers.push(user.uid);
+                }
+              });
+            });
+            return;
+          }
+          if (myMission.likeMembers.includes(user.uid)) {
+
+            db.collection('missions').doc(myMission.id).update({
+              likeCount: firebase.firestore.FieldValue.increment(-1),
+              likeMembers: firebase.firestore.FieldValue.arrayRemove(user.uid),
+            }).then(() => {
+              Materialize.toast(`You disliked this mission`, 3000);
+              this.missions.map(mission => {
+                if (mission.id === myMission.id) {
+                  mission.likeCount--;
+                  const index = mission.likeMembers.indexOf(user.uid);
+                  mission.likeMembers.splice(index, 1);
+                }
+              });
+            });
+            return;
+          }
+          /* db.collection('missions').doc(myMission.id).update({
+            likeCount: firebase.firestore.FieldValue.increment(1)
+          }).then(() => {
+            Materialize.toast(`You liked Mission`, 3000);
+          }); */
+        },
+        getData: function () {
+          var dataQuery;
+          if (this.filterIndex > 0) {
+            dataQuery = db.collection('missions').where('is_public', '==', true).orderBy('likeCount', 'desc');
+          } else {
+            dataQuery = db.collection('missions').where('is_public', '==', true)
+          }
+          
+          dataQuery.get().then((v) => {
+            if (!v.empty) {
+              this.missions = v.docs
+                .map(v => ({
+                  id: v.ref.id,
+                  ...v.data()
+                }))
+                .map(v => ({
+                  ...v,
+                  createdAtTime: v.createdAt ? new Date(v.createdAt.toDate()) : new Date(),
+                  createdAt: moment(v.createdAt.toDate()).format('LLL'),
+                  createdAtShort: moment(v.createdAt.toDate()).format('l LT')
+                }))
+                .filter(v => {
+                  if (v.aircraft === aircraft) {
+                    return true;
+                  }
+                  return false;
+                });
+              if (this.filterIndex < 1) {
+                this.missions.sort((a, b) => a.createdAtTime > b.createdAtTime ? -1 : 1);
+              }
+              for (var i = 0; i < this.missions.length; i++) {
+                cardColors.push(randomColors[Math.floor(Math.random() * 8)]);
+              }
+            }else{
+              this.missions = [];
+            }
+          })
+        }
+      }
+    });
+
+    new Vue({
+      el: '#adminSearch',
+      template: `
+        <template v-if="missions">
+          <div>
+            <div class="container">
+              <div class="row">
+                <form action="#">
+                  <p>
+                    <input class="with-gap" name="group1" type="radio" id="recenMissionFirst" checked />
+                    <label for="recenMissionFirst">Search Users By Email</label>
+                    <input class="with-gap" name="group1" type="radio" id="highRatingFirst" />
+                    <label for="highRatingFirst">Search Missions By Title</label>
                   </p>
                 </form>
               </div>
