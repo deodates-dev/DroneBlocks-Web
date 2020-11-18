81fb1a6e5eeb0b273eac56c37e74d8589e18ef4a
Dennis Baldwin <dennisbaldwin@gmail.com>
2020-11-18 13:56:50 -0600

    Generators file so that we can display functions in Show Mission Code

--- src/blockly/generators/python/tello.js
+++ src/blockly/generators/python/tello.js
@@ -157,3 +157,86 @@ Blockly.Python['variables_set'] = function(block) {
       Blockly.Variables.NAME_TYPE);
   return varName + ' = ' + argument0 + ';\n';
 };
+
+/*
+ * This code below was added on 11/18/2020 to support the new functions category block
+ * It was pulled from the JavaScript procedures.js generator so that we can "Show Mission Code" in a JS format
+*/
+Blockly.Python['procedures_defreturn'] = function(block) {
+  // Define a procedure with a return value.
+  var funcName = Blockly.Python.variableDB_.getName(
+      block.getFieldValue('NAME'), Blockly.Procedures.NAME_TYPE);
+  var branch = Blockly.Python.statementToCode(block, 'STACK');
+  if (Blockly.Python.STATEMENT_PREFIX) {
+    branch = Blockly.Python.prefixLines(
+        Blockly.Python.STATEMENT_PREFIX.replace(/%1/g,
+        '\'' + block.id + '\''), Blockly.Python.INDENT) + branch;
+  }
+  if (Blockly.Python.INFINITE_LOOP_TRAP) {
+    branch = Blockly.Python.INFINITE_LOOP_TRAP.replace(/%1/g,
+        '\'' + block.id + '\'') + branch;
+  }
+  var returnValue = Blockly.Python.valueToCode(block, 'RETURN',
+      Blockly.Python.ORDER_NONE) || '';
+  if (returnValue) {
+    returnValue = '  return ' + returnValue + ';\n';
+  }
+  var args = [];
+  for (var i = 0; i < block.arguments_.length; i++) {
+    args[i] = Blockly.Python.variableDB_.getName(block.arguments_[i],
+        Blockly.Variables.NAME_TYPE);
+  }
+  var code = 'function ' + funcName + '(' + args.join(', ') + ') {\n' +
+      branch + returnValue + '}';
+  code = Blockly.Python.scrub_(block, code);
+  // Add % so as not to collide with helper functions in definitions list.
+  Blockly.Python.definitions_['%' + funcName] = code;
+  return null;
+};
+
+// Defining a procedure without a return value uses the same generator as
+// a procedure with a return value.
+Blockly.Python['procedures_defnoreturn'] =
+    Blockly.Python['procedures_defreturn'];
+
+Blockly.Python['procedures_callreturn'] = function(block) {
+  // Call a procedure with a return value.
+  var funcName = Blockly.Python.variableDB_.getName(
+      block.getFieldValue('NAME'), Blockly.Procedures.NAME_TYPE);
+  var args = [];
+  for (var i = 0; i < block.arguments_.length; i++) {
+    args[i] = Blockly.Python.valueToCode(block, 'ARG' + i,
+        Blockly.Python.ORDER_NONE) || 'null';
+  }
+  var code = funcName + '(' + args.join(', ') + ')';
+  return [code, Blockly.Python.ORDER_FUNCTION_CALL];
+};
+
+Blockly.Python['procedures_callnoreturn'] = function(block) {
+  // Call a procedure with no return value.
+  var funcName = Blockly.Python.variableDB_.getName(
+      block.getFieldValue('NAME'), Blockly.Procedures.NAME_TYPE);
+  var args = [];
+  for (var i = 0; i < block.arguments_.length; i++) {
+    args[i] = Blockly.Python.valueToCode(block, 'ARG' + i,
+        Blockly.Python.ORDER_NONE) || 'null';
+  }
+  var code = funcName + '(' + args.join(', ') + ');\n';
+  return code;
+};
+
+Blockly.Python['procedures_ifreturn'] = function(block) {
+  // Conditionally return value from a procedure.
+  var condition = Blockly.Python.valueToCode(block, 'CONDITION',
+      Blockly.Python.ORDER_NONE) || 'false';
+  var code = 'if (' + condition + ') {\n';
+  if (block.hasReturnValue_) {
+    var value = Blockly.Python.valueToCode(block, 'VALUE',
+        Blockly.Python.ORDER_NONE) || 'null';
+    code += '  return ' + value + ';\n';
+  } else {
+    code += '  return;\n';
+  }
+  code += '}\n';
+  return code;
+};
