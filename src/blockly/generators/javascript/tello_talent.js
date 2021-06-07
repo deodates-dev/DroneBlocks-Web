2c831a861c068f85fbe0746a956af53b0bc38e5b
Dennis Baldwin <dennisbaldwin@gmail.com>
2021-06-07 10:47:39 -0500

    Led with RGB values

--- src/blockly/generators/javascript/tello_talent.js
+++ src/blockly/generators/javascript/tello_talent.js
@@ -9,13 +9,41 @@ function hexToRgb(hex) {
 
 Blockly.JavaScript['main_led_color'] = function(block) {
   let hexColor = block.getFieldValue('color');
-  let blockId = encodeURIComponent(block.id)
-
-  let blockString = 'mission+="|main_led_color,';
   let rgbColor = hexToRgb(hexColor);
+  let blockString = 'mission+="|main_led_color,';
 
   blockString += rgbColor.r + ',' + rgbColor.g + ',' + rgbColor.b;
-  blockString += "," + encodeURIComponent(block.id)
+  blockString += "," + encodeURIComponent(block.id);
+  blockString += '";';
+
+  return blockString;
+};
+
+Blockly.JavaScript['main_led_color_rgb'] = function(block) {
+  let red = Blockly.JavaScript.valueToCode(block, 'red', Blockly.JavaScript.ORDER_NONE);
+  let green = Blockly.JavaScript.valueToCode(block, 'green', Blockly.JavaScript.ORDER_NONE);
+  let blue = Blockly.JavaScript.valueToCode(block, 'blue', Blockly.JavaScript.ORDER_NONE);
+  let blockString = 'mission+="|main_led_color,';
+
+  if(isNaN(parseInt(red))) {
+    blockString += '" + eval(' + red + ') + "';
+  } else {
+    blockString += red;
+  }
+
+  if(isNaN(parseInt(green))) {
+    blockString += '," + eval(' + green + ') + "';
+  } else {
+    blockString += ',' + green;
+  }
+
+  if(isNaN(parseInt(blue))) {
+    blockString += '," + eval(' + blue + ') + "';
+  } else {
+    blockString += ',' + blue;
+  }
+  
+  blockString += "," + encodeURIComponent(block.id);
   blockString += '";';
 
   return blockString;
--- src/blockly/generators/python/tello_talent.js
+++ src/blockly/generators/python/tello_talent.js
@@ -2,11 +2,11 @@ Blockly.Python['main_led_color'] = function(block) {
   var hexColor = block.getFieldValue('color');
   let rgbColor = hexToRgb(hexColor);
   return "setMainLedColor(" + rgbColor.r + " ," + rgbColor.g + " ," + rgbColor.b + ");\n";
-  return code;
 };
 
-
-Blockly.Python['video_duration'] = function(block) {
-  var duration = Blockly.JavaScript.valueToCode(block, 'duration', Blockly.JavaScript.ORDER_NONE);
-  return "video('start', " + duration + ");\n";
+Blockly.Python['main_led_color_rgb'] = function(block) {
+  let red = Blockly.Python.valueToCode(block, 'red', Blockly.JavaScript.ORDER_NONE);
+  let green = Blockly.Python.valueToCode(block, 'green', Blockly.JavaScript.ORDER_NONE);
+  let blue = Blockly.Python.valueToCode(block, 'blue', Blockly.JavaScript.ORDER_NONE);
+  return "setMainLedColor(" + red + " ," + green + " ," + blue + ");\n";
 };
